4c522b2d319576097cc05156859d59a4c3cd9e3a
Masoud Sh <zzmasoud@gmail.com>
2022-11-11 20:54:12 +0100

    Add base view controllers to storyboards

--- iContacts/Controllers/FirstViewController.swift
+++ iContacts/Controllers/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  iContacts
 //
 //  Created by Masoud Sheikh Hosseini on 11/11/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
