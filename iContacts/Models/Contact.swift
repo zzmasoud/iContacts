63d507e08b0454c1efb9d9aa43ab713cf62f54ca
Masoud Sh <zzmasoud@gmail.com>
2022-11-12 09:15:47 +0100

    Refactor `ContactRepresentable` to new struct `Contact` and gather related enums inside it

---
+++ iContacts/Models/Contact.swift
@@ -0,0 +1,45 @@
+//
+//  Contact.swift
+//  iContacts
+//
+//  Created by Masoud Sheikh Hosseini on 11/11/22.
+//
+
+import Foundation
+
+struct Contact: Decodable {
+    
+    enum ContactStatus: String, Decodable {
+        case online, away, busy, offline, callforwarding, pending
+        
+        var icon: String {
+            switch self {
+            case .callforwarding:
+                return "contacts_list_call_forward"
+            default:
+                return "contacts_list_status_\(self)"
+            }
+        }
+    }
+
+    enum Gender: String, Decodable {
+        case male, female, unknown
+        
+        var image: String {
+            return "contacts_list_avatar_\(self)"
+        }
+    }
+
+    var firstName: String
+    var lastName: String
+    var gender: Gender?
+    var status: ContactStatus
+    var message: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case status = "statusIcon"
+        case message = "statusMessage"
+        
+        case firstName, lastName, gender
+    }
+}
